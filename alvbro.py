a14b51244ceb1f0640e4b2eb861e724ea1143fcc
diegoghoul <61204898+diegoghoul@users.noreply.github.com>
2020-09-07 14:55:58 -0600

    segundo commit

--- alvbro.py
+++ alvbro.py
@@ -1,1 +1,9 @@
-print("ya estoy arto :'(")
+print("ya estoy arto :'( \nEste programa es para para ver si su numero es mayor que 10")
+
+numero=int(input("Por favor ingrese un numero : "))
+if numero>10:
+    print("Su numero es mayor que 10")
+elif numero==10:
+    print("Su numero es igual al 10")
+else:
+    print("Su numero es menor que 10")
